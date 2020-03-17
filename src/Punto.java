8ea39394ae71c5a84d0a45500ee64616b086ddcb
Angy Ortiz Mesa <acortizm@unal.edu.co>
2020-03-17 06:55:38 -0500

    respaldo no funcional
    no sirve pero voy a respaldarlo

---
+++ src/Punto.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Angy la mejor del mundo
+ */
+public class Punto {
+    boolean caminable=true,galleta=false,obstaculo=false;
+    int x, y;
+    public Punto(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+}
--- src/Tablero.java
+++ src/Tablero.java
@@ -6,12 +6,24 @@ import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.util.Deque;
 import java.util.Scanner;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.Timer;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
 
 public class Tablero extends JPanel implements ActionListener {
+
+    //mapa de puntos con atributos de obstaculo,caminable,moneda
+    HashMap<String, Punto> coordenadas = new HashMap<String, Punto>();
+
+    //fila de cambios en puntos
+    Queue<String> filaCambios = new LinkedList<String>();
 
     Scanner entrada = new Scanner(System.in);
 
@@ -98,6 +110,129 @@ public class Tablero extends JPanel implements ActionListener {
             addKeyListener(new listenerJugador2());
         }
 
+        //generando galletas
+        for (int i = 0; i <= superiorx; i++) {
+            for (int j = 0; j <= superiory; j++) {
+                String tupla = "(" + i + "," + j + ")";
+
+                if ((i % 30 == 0) && (j % 30 == 0)) {
+                    Punto coordenada = new Punto(i, j);
+                    coordenada.galleta = true;
+                    coordenadas.put(tupla, coordenada);
+                    filaCambios.add(tupla);
+                }
+
+            }
+        }
+        // creando obstaculos
+        System.out.println("Ingrese la cantidad de obstaculos que desea: ");
+        int numeroObstaculos = 0;
+        numeroObstaculos = entrada.nextInt();
+        if (numeroObstaculos > 0) {
+            System.out.println("Las celdas inician en 1 en la esquina superior izquierda y\n"
+                    + "aumentan positivamente hacia la derecha y hacia abajo");
+            for (int i = 0; i < numeroObstaculos; i++) {
+                System.out.println("Obstaculo " + i + 1 + ": ");
+                System.out.println("Ingrese la x de la coordenada inicial: ");
+                int xInicial = 30 * entrada.nextInt();
+                System.out.println("Ingrese la y de la coordenada inicial: ");
+                int yInicial = 30 * entrada.nextInt();
+                System.out.println("Ingrese la x de la coordenada final: ");
+                int xFinal = 30 * entrada.nextInt();
+                System.out.println("Ingrese la y de la coordenada final: ");
+                int yFinal = 30 * entrada.nextInt();
+
+                //generando los puntos
+                int xMenor = 0;
+                int xMayor = 0;
+                int yMenor = 0;
+                int yMayor = 0;
+                if (xInicial >= xFinal) {
+                    xMenor = xFinal;
+                    xMayor = xInicial;
+                } else {
+                    xMenor = xInicial;
+                    xMayor = xFinal;
+                }
+                if (yInicial >= yFinal) {
+                    yMenor = yFinal;
+                    yMayor = yInicial;
+                } else {
+                    yMenor = yInicial;
+                    yMayor = yFinal;
+                }
+
+                //generando L
+                for (int j = xMenor; j <= xMayor; j++) {
+                    String tupla = "(" + j + "," + yMenor + ")";
+                    if (coordenadas.containsKey(tupla)) {
+                        Punto punto = coordenadas.get(tupla);
+                        punto.caminable = false;
+                        punto.galleta = false;
+                        punto.obstaculo = true;
+                        filaCambios.add(tupla);
+                    } else {
+                        Punto punto = new Punto(j, yMenor);
+                        punto.caminable = false;
+                        punto.galleta = false;
+                        punto.obstaculo = true;
+                        filaCambios.add(tupla);
+                    }
+                }
+                //generando S
+                for (int j = yMenor; j <= yMayor; j++) {
+                    String tupla = "(" + xMenor + "," + j + ")";
+
+                    if (coordenadas.containsKey(tupla)) {
+                        Punto punto = coordenadas.get(tupla);
+                        punto.caminable = false;
+                        punto.galleta = false;
+                        punto.obstaculo = true;
+                        filaCambios.add(tupla);
+                    } else {
+                        Punto punto = new Punto(xMenor, j);
+                        punto.caminable = false;
+                        punto.galleta = false;
+                        punto.obstaculo = true;
+                        filaCambios.add(tupla);
+                    }
+                }//generando P
+                for (int j = xMenor; j <= xMayor; j++) {
+                    String tupla = "(" + j + "," + yMayor + ")";
+                    if (coordenadas.containsKey(tupla)) {
+                        Punto punto = coordenadas.get(tupla);
+                        punto.caminable = false;
+                        punto.galleta = false;
+                        punto.obstaculo = true;
+                        filaCambios.add(tupla);
+                    } else {
+                        Punto punto = new Punto(j, yMayor);
+                        punto.caminable = false;
+                        punto.galleta = false;
+                        punto.obstaculo = true;
+                        filaCambios.add(tupla);
+                    }
+
+                }
+                //generando zona no caminable
+                for (int j = xMenor + 1; j < xMayor; j++) {
+                    for (int k = yMenor; yMayor < 10; k++) {
+                        String tupla = "(" + j + "," + k + ")";
+                        if (coordenadas.containsKey(tupla)) {
+                            Punto punto = coordenadas.get(tupla);
+                            punto.caminable = false;
+                            punto.galleta = false;
+                            punto.obstaculo = false;
+                            filaCambios.add(tupla);
+                        } else {
+
+                        }
+                    }
+                }
+
+            }
+        }
+
         setFocusable(true);
         requestFocusInWindow();
 
@@ -117,7 +252,33 @@ public class Tablero extends JPanel implements ActionListener {
             pacman.mover();
             g.drawImage(pacman.imagen, pacman.x, pacman.y, this);
         }
-
+        //recorriendo hasmap para dibujar puntos
+        Iterator it = coordenadas.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
+            //System.out.println(pair.getKey() + " = " + pair.getValue());
+            Punto punto =(Punto) pair.getValue();
+            g.drawImage(fantasma1.imagen, punto.x, punto.y, this);
+            it.remove(); // avoids a ConcurrentModificationException
+        }
+        //actualizando los puntos del mapa desde la pila de cambios
+        /*while(!filaCambios.isEmpty()){
+            String tupla=filaCambios.poll(); 
+            Punto punto = coordenadas.get(tupla);
+                if (punto.galleta) {
+                    g.drawOval(punto.x, punto.y, 3, 3);
+                }
+        }*/
+        //actualizacion recorriendo hashmap
+        for (int i = 0; i <= superiorx; i++) {
+            for (int j = 0; j <= superiory; j++) {
+                String tupla = "("+i+","+j+")";
+                Punto punto = coordenadas.get(tupla);
+                if (punto.galleta) {
+                    g.drawOval(i, j, 100, 100);
+                }
+            }
+        }
         g.drawImage(fantasma1.imagen, fantasma1.x, fantasma1.y, this);
         g.drawImage(fantasma2.imagen, fantasma2.x, fantasma2.y, this);
 
@@ -135,6 +296,7 @@ public class Tablero extends JPanel implements ActionListener {
 
     }
 
+    //controles
     private class listenerJugador1 extends java.awt.event.KeyAdapter {
 
         Pacman pacman = jugadores[0].pacman;
